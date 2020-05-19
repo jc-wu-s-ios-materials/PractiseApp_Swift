1390e0f2f90365ab8f26cae93f499da45da09eff
wujc <690885595@qq.com>
2020-05-19 14:15:02 +0800

    Initial Commit

---
+++ PractiseApp_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PractiseApp_Swift
+//
+//  Created by 吴京城 on 2020/5/19.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
