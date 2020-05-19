222d294af2121a6f4548e63f87e019a58788fc55
wujc <690885595@qq.com>
2020-05-19 15:08:17 +0800

    创建 用于练习 ScrollView 的VC

---
+++ PractiseApp_Swift/UIPractise/ScrollViewPractiseVC.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollViewPractiseVC.swift
+//  PractiseApp_Swift
+//
+//  Created by 吴京城 on 2020/5/19.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewPractiseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("Here is ScrollViewPractiseVC")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
