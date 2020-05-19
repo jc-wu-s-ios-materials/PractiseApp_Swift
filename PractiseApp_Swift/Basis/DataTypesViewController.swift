25d3f73465014f6676da48713d820e92be561dfb
wujc <690885595@qq.com>
2020-05-19 15:02:47 +0800

    创建 基础类型vc

---
+++ PractiseApp_Swift/Basis/DataTypesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DataTypesViewController.swift
+//  PractiseApp_Swift
+//
+//  Created by 吴京城 on 2020/5/19.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+import UIKit
+
+
+class DataTypesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("Here is DataTypesViewController!")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
